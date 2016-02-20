0ff5776d362943ed372138268b94798653ff1b2a
Qiwei <liqiwei1224@gmail.com>
2016-02-20 13:42:58 -0800

    fixed bugs

---
+++ builtInDists.R
@@ -0,0 +1,61 @@
+# sustitute strings into actual functions
+subst=function(obj){
+  for(i in 1:length(obj$g)){
+    if(class(obj$g[[i]])=="character"){
+      obj$g[[i]] = buildInDists(obj$g[[i]])
+    }
+  }
+  return(obj)
+}
+
+# built in distributions
+builtInDists = function(name){
+  if(name == "poisson"){
+    g = function(th,x){
+      t1=th[1]
+      meanb=t1
+      m1=meanb-x
+      f=cbind(m1)
+      return(f)
+    }
+  }
+  
+  if(name == "power law"){
+
+  }
+  
+  if(name == "gamma"){
+    g = function(th,x){
+      k=th[1]
+      theta=th[2]
+      meanb=k*theta
+      m1=meanb-x
+      m2=k*theta^2-(x-meanb)^2
+      f=cbind(m1,m2)
+      return(f)
+    }
+  }
+  
+  if(name == "beta"){
+    g = function(th,x){
+      t1=th[1]
+      t2=th[2]
+      t12=t1+t12
+      meanb=t1/t12
+      m1=meanb-x
+      m2=t1*t2/(t12^2*(t12+1))-(x-meanb)^2
+      f=cbind(m1,m2)
+      return(f)
+    }
+    
+  }
+  
+  if(name == "mixture of 2 poissons"){
+    
+  }
+  
+  if(name == "mixture of 2 exponentials"){
+    
+  }
+  return(g)
+}
--- mmfit.R
+++ mmfit.R
@@ -1,72 +1,19 @@
 mmfit=function(g,x,start){
-  ####### Check Input #######
-  if (ncol(x)==length(g)){
-    obj=structure(list(g=g,x=x,start=start),class=c("mmfit","list"))
-  } else if (length(g)==1){
-    g=rep(g,ncol(x))
-    obj=structure(list(g=g,x=x,start=start),class=c("mmfit","list"))
-  } else {
-    stop("Error: g is not sufficiently defined.")  
+  if(is.null(g) || is.null(x) || is.null(start)){
+    stop("ERROR: input cannot be null")
   }
-  ####### Substitute #######
-  for (i in 1:length(obj$g)){
-    if (class(obj$g[[i]])=="character") {
-      obj$g[[i]]=subst(obj$g[[i]])
-      #obj$g[i]="aa"
-    }
+  
+  if(ncol(x)==length(g)){
+    obj = structure(list(g=g,x=x,start=start), class=c("mmfit","list"))
+    obj = subst(obj)
+  } else if (length(g)==1){
+    g = as.list(rep(g, ncol(x)))
+    obj = structure(list(g=g,x=x,start=start), class=c("mmfit","list"))
+    obj = subst(obj)
+  } else{
+    stop("Error: g is not sufficiently defined.")  
   }
   return(obj)
 }
 
-####### Sustitute Function & G-Function Library #######
-subst=function(g){
-  func=switch(g,poisson=POISSON,powerlaw=POWERLAW,gamma=GAMMA,beta=BETA)
-  return(func) 
-}
 
-####### POISSON #######
-POISSON=function(th,x){
-  t1=th[1]
-  meanb=t1
-  m1=meanb-x
-  f=cbind(m1)
-  return(f)
-}
-
-##### f(x) = a x^(-k) #####
-POWERLAW=function(th,x){
-##### Lacking of well-defined value #####
-}
-
-##### GAMMA #####
-GAMMA=function(th,x){
-  k=th[1]
-  theta=th[2]
-  meanb=k*theta
-  m1=meanb-x
-  m2=k*theta^2-(x-meanb)^2
-  f=cbind(m1,m2)
-  return(f)
-}
-
-#####  BETA  #####
-BETA=function(th,x){
-  t1=th[1]
-  t2=th[2]
-  t12=t1+t12
-  meanb=t1/t12
-  m1=meanb-x
-  m2=t1*t2/(t12^2*(t12+1))-(x-meanb)^2
-  f=cbind(m1,m2)
-  return(f)
-}
-
-#####  Mixture 2 Poisson  #####
-
-
-#####  Mixture 2 Exponetial #####
-
-####### Example #######
-#> source("mmfit.R")
-#> mmfit(list("beta"),x,start)
-# then the obj will be returned
