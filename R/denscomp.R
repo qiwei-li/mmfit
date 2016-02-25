e089bc3f107298214ef54303a09430230b59dd0b
jpzhangvincent <vincentzhangv@gmail.com>
2016-02-25 10:04:37 -0800

    adjust density plot

--- R/denscomp.R
+++ R/denscomp.R
@@ -2,13 +2,17 @@ do_denscomp = function(g, x, thetahat gd){
   if(!is.null(gd)){
     values = seq(from = min(x), to = max(x), by = 0.1)
     prob = sapply(values, function(i) gd(x, thetahat))
-    fhat = sample(x = c(min(x), max(x)), 5000, replace = TRUE, prob = prob)
+    fhat = sample(values, len(x), replace = TRUE, prob = prob)
   }
   
   if(g == "poisson"){
-    fhat = rpois(1000,thetahat[1])
+    fhat = rpois(len(x),thetahat[1])
   }
-   
+  
+  if(g == "negative binomial"){
+    fhat = rnbinom(len(x),thetahat[1])
+  }
+  
   if(g == "power law"){
     gamma = thetahat[1]
     test.sum = 0
@@ -28,22 +32,22 @@ do_denscomp = function(g, x, thetahat gd){
   }
   
   if(g == "gamma"){
-    fhat = rgamma(1000,thetahat[1], thetahat[2])
+    fhat = rgamma(len(x),thetahat[1], thetahat[2])
   }
   
   if(g == "beta"){
-    fhat = rbeta(1000,thetahat[1], thetahat[2])
+    fhat = rbeta(len(x),thetahat[1], thetahat[2])
   }
   
   if(g == "mixture of 2 poissons"){
-    flag = sample(2, 1000, prob = c(thetahat[3], 1-thetahat[3]), replace=TRUE)
+    flag = sample(2, len(x), prob = c(thetahat[3], 1-thetahat[3]), replace=TRUE)
     d1 = rpois(sum(flag==1), thetahat[1])
     d2 = rpois(sum(flag==2), thetahat[1])
     fhat = c(d1, d2)
   }
   
   if(g == "mixture of 2 exponentials"){
-    flag = sample(2, 1000, prob = c(thetahat[3], 1-thetahat[3]), replace=TRUE)
+    flag = sample(2, len(x), prob = c(thetahat[3], 1-thetahat[3]), replace=TRUE)
     d1 = rexp(sum(flag==1), thetahat[1])
     d2 = rexp(sum(flag==2), thetahat[1])
     fhat = c(d1, d2)
